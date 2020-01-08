c830858e07d5528cde4e4bf1bb318619f3da8685
Jun Shao <jshao@futurewei.com>
2020-01-08 15:30:14 -0800

    Updated the script to set up kube-apiserver with apiserver.config (#365)

--- hack/alkaid-apiserver-partition.sh
+++ hack/alkaid-apiserver-partition.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # Copyright 2014 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,57 +24,6 @@ CERT_DIR=${CERT_DIR:-"/var/run/kubernetes"}
 
 KUBECTL=${KUBECTL:-"${KUBE_ROOT}/cluster/kubectl.sh"}
 
-function detect_binary {
-    # Detect the OS name/arch so that we can find our binary
-    case "$(uname -s)" in
-      Darwin)
-        host_os=darwin
-        ;;
-      Linux)
-        host_os=linux
-        ;;
-      *)
-        echo "Unsupported host OS.  Must be Linux or Mac OS X." >&2
-        exit 1
-        ;;
-    esac
-
-    case "$(uname -m)" in
-      x86_64*)
-        host_arch=amd64
-        ;;
-      i?86_64*)
-        host_arch=amd64
-        ;;
-      amd64*)
-        host_arch=amd64
-        ;;
-      aarch64*)
-        host_arch=arm64
-        ;;
-      arm64*)
-        host_arch=arm64
-        ;;
-      arm*)
-        host_arch=arm
-        ;;
-      i?86*)
-        host_arch=x86
-        ;;
-      s390x*)
-        host_arch=s390x
-        ;;
-      ppc64le*)
-        host_arch=ppc64le
-        ;;
-      *)
-        echo "Unsupported host arch. Must be x86_64, 386, arm, arm64, s390x or ppc64le." >&2
-        exit 1
-        ;;
-    esac
-
-   GO_OUT="${KUBE_ROOT}/_output/local/bin/${host_os}/${host_arch}"
-}
 
 function apply_test_data {
   echo "Creating apiserver partion test data"
@@ -86,19 +36,16 @@ function apply_test_data {
   echo "Apiserver partion test data have been successfully applied."
 }
 
-
 function start_apiserver {
-
-  if [ "${GO_OUT}" == "" ]; then
-    detect_binary
-  fi
 
   echo "Copying apiserver.config..."
 
-  cp "${KUBE_ROOT}/test/conf/apiserver-${APISERVER_PARTITION_TENANT}.config" "${GO_OUT}/apiserver.config"
+  cp "${KUBE_ROOT}/test/conf/apiserver-${APISERVER_PARTITION_TENANT}.config" "apiserver.config"
 
+  echo "Running alkaid-up.sh..."
   source $(dirname "$0")/alkaid-up.sh
 }
 
 "$@"
+
 
