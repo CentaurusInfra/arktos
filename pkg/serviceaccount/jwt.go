18c4758fa8d1bcdd563bd44ceaab87ac619a50be
Peng Du <pdu@futurewei.com>
2020-03-30 20:49:55 -0700

    add tenant extraction to jwt bearer token authentication used by service account (#145)

--- pkg/serviceaccount/jwt.go
+++ pkg/serviceaccount/jwt.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2014 The Kubernetes Authors.
+Copyright 2020 Authors of Arktos - file modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -37,8 +38,10 @@ import (
 // ServiceAccountTokenGetter defines functions to retrieve a named service account and secret
 type ServiceAccountTokenGetter interface {
 	GetServiceAccount(namespace, name string) (*v1.ServiceAccount, error)
+	GetServiceAccountWithMultiTenancy(tenant, namespace, name string) (*v1.ServiceAccount, error)
 	GetPod(namespace, name string) (*v1.Pod, error)
 	GetSecret(namespace, name string) (*v1.Secret, error)
+	GetSecretWithMultiTenancy(tenant, namespace, name string) (*v1.Secret, error)
 }
 
 type TokenGenerator interface {
