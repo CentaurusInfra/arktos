54e81c481ece4df88463221e7eaf55745dee5e6c
qianchen <chenqianfzh@yahoo.com>
2020-04-23 23:34:07 -0700

    fix

--- test/integration/apiextension/defaulting_test.go
+++ test/integration/apiextension/defaulting_test.go
@@ -1,6 +1,5 @@
 /*
 Copyright 2019 The Kubernetes Authors.
-Copyright 2020 Authors of Arktos - file modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
