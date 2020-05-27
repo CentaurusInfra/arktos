dedd755ef561646eb5004cf6ef0a9c5ac96f4595
Yunwen Bai <yunwen.bai@futurewei.com>
2020-05-27 21:32:59 +0000

    Kuberuntime image API rework

--- pkg/kubelet/images/image_manager.go
+++ pkg/kubelet/images/image_manager.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2016 The Kubernetes Authors.
+Copyright 2020 Authors of Arktos - file modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -101,7 +102,7 @@ func (m *imageManager) EnsureImageExists(pod *v1.Pod, container *v1.Container, p
 		return "", msg, ErrInvalidImageName
 	}
 
-	spec := kubecontainer.ImageSpec{Image: image}
+	spec := kubecontainer.ImageSpec{Image: image, Pod: pod}
 	imageRef, err := m.imageService.GetImageRef(spec)
 	if err != nil {
 		msg := fmt.Sprintf("Failed to inspect image %q: %v", container.Image, err)
