4f85dc4a62d2b2c913f5b6e52d078355b0a8848b
changhong <hchang@futurewei.com>
2020-06-12 02:46:57 +0000

    Try again

--- hack/make-rules/update.sh
+++ hack/make-rules/update.sh
@@ -83,7 +83,7 @@ if [ "${JENKINS_HOME:-}" != "" ]; then
 	echo "Under Jenkins CI environment, check whether there is file updated."
 	echo "We define return value as 0 if there is no file updated, and 1 if there is file updated."
 	MODIFIED_FILES=$(git status | grep modified:) || true
-	if [[ "${MODIFIED_FILES:-}" != "" && "${MODIFIED_FILES:-}" != 'modified: api/openapi-spec/swagger.json' ]]; then
+	if [[ "${MODIFIED_FILES:-}" != "" && "${MODIFIED_FILES:-}" != "	modified:   api/openapi-spec/swagger.json" ]]; then
 		echo "Following files are updated:"
 		echo $MODIFIED_FILES
 		exit 1
