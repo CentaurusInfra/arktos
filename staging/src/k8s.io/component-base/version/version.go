787b5fdf4a4ef4be6b3a9c4041b90a5233122c29
Ying Huang <sindica2000@yahoo.com>
2020-07-30 18:47:42 +0000

    Pick up k8s.io/component-base/metrics & version from perf-test clusterloader2/vendor (4/9/2020 - d9a552b4f2e00dfc4c12e74eddae0a5e10aeed71)

--- staging/src/k8s.io/component-base/version/version.go
+++ staging/src/k8s.io/component-base/version/version.go
@@ -14,37 +14,29 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package metrics
+package version
 
 import (
 	"fmt"
-	"regexp"
-
-	"github.com/blang/semver"
+	"runtime"
 
 	apimachineryversion "k8s.io/apimachinery/pkg/version"
 )
 
-const (
-	versionRegexpString = `^v(\d+\.\d+\.\d+)`
-)
-
-var (
-	versionRe = regexp.MustCompile(versionRegexpString)
-)
-
-func parseSemver(s string) *semver.Version {
-	if s != "" {
-		sv := semver.MustParse(s)
-		return &sv
+// Get returns the overall codebase version. It's for detecting
+// what code a binary was built from.
+func Get() apimachineryversion.Info {
+	// These variables typically come from -ldflags settings and in
+	// their absence fallback to the settings in ./base.go
+	return apimachineryversion.Info{
+		Major:        gitMajor,
+		Minor:        gitMinor,
+		GitVersion:   gitVersion,
+		GitCommit:    gitCommit,
+		GitTreeState: gitTreeState,
+		BuildDate:    buildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
-	return nil
-}
-func parseVersion(ver apimachineryversion.Info) semver.Version {
-	matches := versionRe.FindAllStringSubmatch(ver.String(), -1)
-
-	if len(matches) != 1 {
-		panic(fmt.Sprintf("version string \"%v\" doesn't match expected regular expression: \"%v\"", ver.String(), versionRe.String()))
-	}
-	return semver.MustParse(matches[0][1])
 }
