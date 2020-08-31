7945b4d77e2ef4179ea8b0a982af69cd5174c216
Hong-Chang <hchang@futurewei.com>
2020-08-31 06:36:18 +0000

    Update mizar-controller-manager description

--- cmd/mizar-controller-manager/app/controllermanager.go
+++ cmd/mizar-controller-manager/app/controllermanager.go
@@ -98,14 +98,8 @@ func NewControllerManagerCommand() *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use: "mizar-controller-manager",
-		Long: `The Kubernetes controller manager is a daemon that embeds
-the core control loops shipped with Kubernetes. In applications of robotics and
-automation, a control loop is a non-terminating loop that regulates the state of
-the system. In Kubernetes, a controller is a control loop that watches the shared
-state of the cluster through the apiserver and makes changes attempting to move the
-current state towards the desired state. Examples of controllers that ship with
-Kubernetes today are the replication controller, endpoints controller, namespace
-controller, and serviceaccounts controller.`,
+		Long: `The Mizar controller manager is a daemon that is watching
+change happenning in cluster, and send to Mizar backend.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			verflag.PrintAndExitIfRequested()
 			utilflag.PrintFlags(cmd.Flags())
