5716d83bc323e1f07444311b169044074b7829cd
Ying Huang <sindica2000@yahoo.com>
2020-09-02 02:30:18 +0000

    Close aggregated watcher when connection was refused for some watchers.

--- staging/src/k8s.io/client-go/tools/cache/reflector.go
+++ staging/src/k8s.io/client-go/tools/cache/reflector.go
@@ -414,6 +414,8 @@ func (r *Reflector) ListAndWatch(stopCh <-chan struct{}) error {
 			if urlError, ok := err.(*url.Error); ok {
 				if opError, ok := urlError.Err.(*net.OpError); ok {
 					if errno, ok := opError.Err.(syscall.Errno); ok && errno == syscall.ECONNREFUSED {
+						klog.Infof("Retry on connection refused error [%v]", err)
+						aggregatedWatcher.Stop()
 						time.Sleep(time.Second)
 						continue
 					}
