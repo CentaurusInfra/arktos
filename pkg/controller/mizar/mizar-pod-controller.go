41f25fea2e218644eb862b5a9cdd8bc99ed211d5
Hong-Chang <hchang@futurewei.com>
2020-09-11 20:29:37 +0000

    Fix issue that no hostIP sent to grpc server

--- pkg/controller/mizar/mizar-pod-controller.go
+++ pkg/controller/mizar/mizar-pod-controller.go
@@ -103,7 +103,7 @@ func (c *MizarPodController) Run(workers int, stopCh <-chan struct{}) {
 
 func (c *MizarPodController) createObj(obj interface{}) {
 	key, _ := controller.KeyFunc(obj)
-	c.queue.Add(KeyWithEventType{Key: key, EventType: EventType_Create})
+	c.queue.Add(KeyWithEventType{Key: key, EventType: EventType_Create, EventTime: time.Now().Format(time.RFC3339Nano)})
 }
 
 // When an object is updated.
@@ -127,7 +127,7 @@ func (c *MizarPodController) updateObj(old, cur interface{}) {
 func (c *MizarPodController) deleteObj(obj interface{}) {
 	key, _ := controller.KeyFunc(obj)
 	klog.Infof("%v deleted. key %s.", controllerForMizarPod, key)
-	c.queue.Add(KeyWithEventType{Key: key, EventType: EventType_Delete})
+	c.queue.Add(KeyWithEventType{Key: key, EventType: EventType_Delete, EventTime: time.Now().Format(time.RFC3339Nano)})
 }
 
 // worker runs a worker thread that just dequeues items, processes them, and marks them done.
