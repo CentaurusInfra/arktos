c608e6d3a8b8523709e22a50a2d3ee8685afea5b
Wang Jun <guaklw@163.com>
2020-09-11 15:17:10 +0800

    chang the solution to real sdp, delete no used handler

--- pkg/cloudgateway/controller/handler/serviceexpose_handler.go
+++ pkg/cloudgateway/controller/handler/serviceexpose_handler.go
@@ -11,38 +11,35 @@ import (
 type ServiceExposeHandler struct {
 	serviceLister listers.EServiceLister
 	siteLister    listers.ESiteLister
-	policyLister  listers.EPolicyLister
-	serverLister  listers.EServerLister
-	gatewayLister listers.EGatewayLister
 	gatewayClient clientset.Interface
 }
 
 // ServiceExposeObj
 type ServiceExposeObj struct {
-	serviceExpose   v1.ServiceExpose
-	service         v1.EService
-	allowedPolicies []PolicyObj
-}
-
-// PolicyObj
-type PolicyObj struct {
-	policy         v1.EPolicy
-	allowedServers []v1.EServer
+	serviceExpose v1.ServiceExpose
+	service       v1.EService
+	serviceSite   v1.ESite
+	clientSite    v1.ESite
 }
 
 // NewServiceExposeHandler creates a new ServiceExposeHandler
-func NewServiceExposeHandler(sl listers.EServiceLister, siteLister listers.ESiteLister,
-	policyLister listers.EPolicyLister, serverLister listers.EServerLister,
-	gatewayLister listers.EGatewayLister) *ServiceExposeHandler {
+func NewServiceExposeHandler(serviceLister listers.EServiceLister, siteLister listers.ESiteLister,
+	gatewayClient clientset.Interface) *ServiceExposeHandler {
 	se := &ServiceExposeHandler{
-		serviceLister: sl,
+		serviceLister: serviceLister,
 		siteLister:    siteLister,
-		policyLister:  policyLister,
-		serverLister:  serverLister,
-		gatewayLister: gatewayLister,
+		gatewayClient: gatewayClient,
 	}
-
 	return se
+}
+
+func (h *ServiceExposeHandler) RequestVirtualPresence(site *v1.ESite, serviceExpose *v1.ServiceExpose) (string, error) {
+	return "", nil
+}
+
+func (h *ServiceExposeHandler) ReleaseVirtualPresence(site *v1.ESite, serviceExpose *v1.ServiceExpose,
+	virtualPresenceIp string) error {
+	return nil
 }
 
 // Handle the service expose request
@@ -57,44 +54,92 @@ func (h *ServiceExposeHandler) ObjectCreated(tenant string, namespace string, ob
 	se := obj.(*v1.ServiceExpose)
 	klog.V(4).Info("ServiceExposeHandler.ObjectCreated: %v", se)
 
-	// Transform the service expose to obj
+	// Get the service expose obj
 	seObj, err := h.getServiceExposeObj(tenant, namespace, se)
 	if err != nil {
 		// Update service expose to wrong status and return
 		// TODO(nkwangjun): update record to detail message
-		nerr := h.updateServiceExposeStatus(seObj, v1.ServiceExposeError, namespace, tenant)
+		nerr := h.updateServiceExposeStatus(se, v1.ServiceExposeError, namespace, tenant)
 		if nerr != nil {
-			klog.Error("UpdateServiceExpose to error failed, %v, err:%v", se, nerr)
+			klog.Error("UpdateServiceExpose to error failed, service expose:%v, err:%v", se, nerr)
 		}
 		return
 	}
 
-	// If service VirtualPresenceIp is not assigned, return error
-	if seObj.service.VirtualPresenceIp == "" {
-		klog.Errorf("Serviceexpose error, service not ready, there is no virtual presence ip, service:%v",
-			seObj.service)
-		// TODO(nkwangjun): update record to detail message
-		nerr := h.updateServiceExposeStatus(seObj, v1.ServiceExposeError, namespace, tenant)
-		if nerr != nil {
-			klog.Error("UpdateServiceExpose to error failed, %v, err:%v", se, nerr)
+	// If service virtual presence ip is assigned, do check and update
+	if seObj.serviceExpose.VirtualPresenceIp != "" {
+		klog.V(4).Infof("ServiceExpose virtual presence ip for service is assigned, se:%v", se)
+		// NOTE(nkwangjun): add check and update here later
+	} else {
+		// Do request virtual presence ip
+		klog.V(4).Infof("ServiceExpose try to get one virtual presence ip in site:%v", seObj.clientSite)
+		virtualPresenceIp, err := h.RequestVirtualPresence(&seObj.clientSite, se)
+		if err != nil {
+			klog.Errorf("Request virtual presence ip for service error, se:%v, err:%v", se, err)
+			// TODO(nkwangjun): update record to detail message
+			nerr := h.updateServiceExposeStatus(se, v1.ServiceExposeError, namespace, tenant)
+			if nerr != nil {
+				klog.Error("UpdateServiceExpose to error failed, %v, err:%v", se, nerr)
+			}
+			return
 		}
-		return
+
+		// Update virtual presence ip for service
+		seCopy := seObj.serviceExpose.DeepCopy()
+		seCopy.VirtualPresenceIp = virtualPresenceIp
+		se, err = h.gatewayClient.CloudgatewayV1().ServiceExposesWithMultiTenancy(namespace, tenant).Update(seCopy)
+		if err != nil {
+			klog.Errorf("Update virtual presence ip for se error, se:%v, err:%v", se, err)
+			// TODO(nkwangjun): update record to detail message
+			err = h.ReleaseVirtualPresence(&seObj.clientSite, se, virtualPresenceIp)
+			if err != nil {
+				klog.Errorf("Release virtual presence for service error, se:%v, err:%v", se, err)
+			}
+			return
+		}
 	}
 
-	for _, policy := range seObj.allowedPolicies {
-		for _, server := range policy.allowedServers {
-			if server.VirtualPresenceIp == "" {
-				klog.Errorf("Serviceexpose error, service not ready, there is no virtual presence ip, "+
-					"server:%v", server)
-				// TODO(nkwangjun):  update record to detail message
-				nerr := h.updateServiceExposeStatus(seObj, v1.ServiceExposeError, namespace, tenant)
+	for _, eClient := range se.AllowedClients {
+		// If eClient virtual presence ip is assigned, do check and update
+		if eClient.VirtualPresenceIp != "" {
+			klog.V(4).Infof("ServiceExpose virtual presence ip for eClient:%s is assigned, se:%v",
+				eClient.Ip, se)
+			// NOTE(nkwangjun): add check and update here later
+		} else {
+			// Do request virtual presence ip
+			klog.V(4).Infof("ServiceExpose try to get one virtual presence ip in site:%v", seObj.serviceSite)
+			virtualPresenceIp, err := h.RequestVirtualPresence(&seObj.serviceSite, se)
+			if err != nil {
+				klog.Errorf("Request virtual presence ip for service error, se:%v, err:%v", se, err)
+				// TODO(nkwangjun): update record to detail message
+				nerr := h.updateServiceExposeStatus(se, v1.ServiceExposeError, namespace, tenant)
 				if nerr != nil {
 					klog.Error("UpdateServiceExpose to error failed, %v, err:%v", se, nerr)
 				}
 				return
 			}
+
+			// Update virtual presence ip for service
+			seCopy := se.DeepCopy()
+			seCopy.VirtualPresenceIp = virtualPresenceIp
+			se, err = h.gatewayClient.CloudgatewayV1().ServiceExposesWithMultiTenancy(namespace, tenant).Update(seCopy)
+			if err != nil {
+				klog.Errorf("Update virtual presence ip for se error, se:%v, err:%v", se, err)
+				// TODO(nkwangjun): update record to detail message
+				nerr := h.ReleaseVirtualPresence(&seObj.serviceSite, se, virtualPresenceIp)
+				if nerr != nil {
+					klog.Errorf("Release virtual presence for service error, se:%v, err:%v", se, nerr)
+				}
+				return
+			}
+
+			klog.V(4).Infof("Request and update virtual presence ip success for eClient, vp:%s, se:%v",
+				virtualPresenceIp, se)
 		}
 	}
+
+	// Update service expose with virtual presence ready
+	seObj.serviceExpose = *se
 
 	// Send traffic flow required info
 	// 1. Send to the service site
@@ -105,12 +150,50 @@ func (h *ServiceExposeHandler) ObjectCreated(tenant string, namespace string, ob
 	h.syncServiceExpose(seObj)
 }
 
-func (h *ServiceExposeHandler) updateServiceExposeStatus(seObj *ServiceExposeObj,
+func (h *ServiceExposeHandler) updateVirtualPresenceForEClient(se *v1.ServiceExpose, eSite *v1.ESite,
+	namespace string, tenant string) error {
+	for _, eClient := range se.AllowedClients {
+		// If eClient virtual presence ip is assigned, do check and update
+		if eClient.VirtualPresenceIp != "" {
+			klog.V(4).Infof("ServiceExpose virtual presence ip for eClient:%s is assigned, se:%v",
+				eClient.Ip, se)
+			// NOTE(nkwangjun): add check and update here later
+		} else {
+			// Do request virtual presence ip
+			klog.V(4).Infof("ServiceExpose try to get one virtual presence ip in site:%v", eSite)
+			virtualPresenceIp, err := h.RequestVirtualPresence(eSite, se)
+			if err != nil {
+				klog.Errorf("Request virtual presence ip for service error, se:%v, err:%v", se, err)
+				return err
+			}
+
+			// Update virtual presence ip for service
+			seCopy := se.DeepCopy()
+			seCopy.VirtualPresenceIp = virtualPresenceIp
+			_, err = h.gatewayClient.CloudgatewayV1().ServiceExposesWithMultiTenancy(namespace, tenant).Update(seCopy)
+			if err != nil {
+				klog.Errorf("Update virtual presence ip for se error, se:%v, err:%v", se, err)
+				// TODO(nkwangjun): update record to detail message
+				nerr := h.ReleaseVirtualPresence(eSite, se, virtualPresenceIp)
+				if nerr != nil {
+					klog.Errorf("Release virtual presence for service error, se:%v, err:%v", se, nerr)
+				}
+				return err
+			}
+
+			klog.V(4).Infof("Request and update virtual presence ip success for eClient, vp:%s, se:%v",
+				virtualPresenceIp, se)
+		}
+	}
+	return nil
+}
+
+func (h *ServiceExposeHandler) updateServiceExposeStatus(serviceExpose *v1.ServiceExpose,
 	phase v1.ServiceExposePhase, namespace string, tenant string) error {
 	// NEVER modify objects from the store. It's a read-only, local cache.
 	// We can use DeepCopy() to make a deep copy of original object and modify this copy
 	// Or create a copy manually for better performance
-	se := seObj.serviceExpose.DeepCopy()
+	se := serviceExpose.DeepCopy()
 	se.Status = v1.ServiceExposeStatus{Phase: phase}
 	_, err := h.gatewayClient.CloudgatewayV1().ServiceExposesWithMultiTenancy(namespace, tenant).Update(se)
 	return err
@@ -130,43 +213,30 @@ func (h *ServiceExposeHandler) getServiceExposeObj(tenant string, namespace stri
 	serviceName := expose.EServiceName
 	service, err := h.serviceLister.EServicesWithMultiTenancy(namespace, tenant).Get(serviceName)
 	if err != nil {
-		klog.Errorf("Get service in service expose error, serviceexpose:%v, service name:%s", expose,
+		klog.Errorf("Get service in service expose error, service expose:%v, service name:%s", expose,
 			serviceName)
 		return nil, err
 	}
 
 	seObj.service = *service
-	policyNameList := expose.EPolicies
-	allowedPolicies := []PolicyObj{}
-	seObj.allowedPolicies = allowedPolicies
-	for _, policyName := range policyNameList {
-		policy, err := h.policyLister.EPoliciesWithMultiTenancy(namespace, tenant).Get(policyName)
-		if err != nil {
-			klog.Errorf("Get policy in service expose to error, serviceexpose:%v, policy name:%s", expose,
-				policyName)
-			return nil, err
-		}
 
-		policyObj := &PolicyObj{
-			policy:         *policy,
-			allowedServers: []v1.EServer{},
-		}
-
-		// check policy server is valid or not
-		for _, serverName := range policy.AllowedServers {
-			server, err := h.serverLister.EServersWithMultiTenancy(namespace, tenant).Get(serverName)
-			if err != nil {
-				klog.Errorf("Get server in service expose policy allowed to error, serviceexpose:%v, "+
-					"policy name:%s, server name:%s", expose, policyName, serverName)
-				return nil, err
-			}
-
-			policyObj.allowedServers = append(policyObj.allowedServers, *server)
-		}
-
-		allowedPolicies = append(allowedPolicies, *policyObj)
+	// Check the site of the service
+	serviceSite, err := h.siteLister.ESitesWithMultiTenancy(namespace, tenant).Get(service.ESiteName)
+	if err != nil {
+		klog.Errorf("Get site from service error, service:%v", service)
+		return nil, err
 	}
 
+	seObj.serviceSite = *serviceSite
+
+	// Check the site of the client
+	clientSite, err := h.siteLister.ESitesWithMultiTenancy(namespace, tenant).Get(expose.ESiteName)
+	if err != nil {
+		klog.Errorf("Get site from expose client error, expose:%v", expose)
+		return nil, err
+	}
+
+	seObj.clientSite = *clientSite
 	return seObj, nil
 }
 
