9932feaf87349a9ea7e1b884d324ef348ffc31d3
Eunju Kim <ekim@futurewei.com>
2020-09-14 11:56:15 -0700

    test9

--- pkg/controller/mizar/mizar-endpoints-controller.go
+++ pkg/controller/mizar/mizar-endpoints-controller.go
@@ -327,14 +327,15 @@ func (c *MizarEndpointsController) gRPCRequest(event EventType, ep ServiceEndpoi
 	defer conn.Close()
 	defer cancel()
 	var portMessage PortsMessage
-	var ports []PortsMessage
+	var ports []*PortsMessage
 	var resource BuiltinsServiceEndpointMessage
 	if (ep.ports) != nil {
 		for i := 0; i < len(ep.ports); i++ {
+			portMessage := PortsMessage{}
 			portMessage.FrontendPort = ep.ports[i].frontPort
 			portMessage.BackendPort = ep.ports[i].backendPort
 			portMessage.Protocol = ep.ports[i].protocol
-			ports = append(ports, portMessage)
+			ports = append(ports, &portMessage)
 			// ports[i].FrontendPort = ep.ports[i].frontPort
 			// ports[i].BackendPort = ep.ports[i].backendPort
 			// ports[i].Protocol = ep.ports[i].protocol
