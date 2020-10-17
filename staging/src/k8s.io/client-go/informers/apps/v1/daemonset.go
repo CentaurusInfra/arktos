640a53f2cdd16cf2b94cd201728d465fb0ad62f2
Ying Huang <sindica2000@yahoo.com>
2020-10-17 02:24:01 +0000

    code generator changes for informer

--- staging/src/k8s.io/client-go/informers/apps/v1/daemonset.go
+++ staging/src/k8s.io/client-go/informers/apps/v1/daemonset.go
@@ -73,7 +73,7 @@ func NewFilteredDaemonSetInformerWithMultiTenancy(client kubernetes.Interface, n
 				}
 				return client.AppsV1().DaemonSetsWithMultiTenancy(namespace, tenant).List(options)
 			},
-			WatchFunc: func(options metav1.ListOptions) watch.AggregatedWatchInterface {
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
