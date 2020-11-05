78f2ad2f472c1c78f2ec7d99cd7629796e099432
Yunwen Bai <yunwen.bai@futurewei.com>
2020-11-05 16:00:48 +0000

    disable nodeipam controller on resource partition

--- hack/lib/common.sh
+++ hack/lib/common.sh
@@ -457,7 +457,7 @@ function kube::common::start_workload_controller_manager {
 function kube::common::start_controller_manager {
     local controller_opts="*,-nodelifecycle,-nodeipam"
     if [ "${IS_RESOURCE_PARTITION}" == "true" ]; then
-       controller_opts="nodelifecycle,nodeipam"
+       controller_opts="nodelifecycle"
     fi
 
     CONTROLPLANE_SUDO=$(test -w "${CERT_DIR}" || echo "sudo -E")
