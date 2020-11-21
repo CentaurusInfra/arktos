d82ed89dc6c708c51c0434a5d15463ec61627a37
Ying Huang <sindica2000@yahoo.com>
2020-11-21 01:27:47 +0000

    add exponential backoff with reset to reflector https://github.com/kubernetes/kubernetes/commit/f1248163ff4dc3cf164284bf255c10b6b538aa66 committed on Feb 7, 2020

--- staging/src/k8s.io/client-go/tools/cache/reflector.go
+++ staging/src/k8s.io/client-go/tools/cache/reflector.go
@@ -82,9 +82,10 @@ type Reflector struct {
 	store Store
 	// listerWatcher is used to perform lists and watches.
 	listerWatcher ListerWatcher
-	// period controls timing between one watch ending and
-	// the beginning of the next one.
-	period       time.Duration
+
+	// backoff manages backoff of ListWatch
+	backoffManager wait.BackoffManager
+
 	resyncPeriod time.Duration
 	ShouldResync func() bool
 	// clock allows tests to manipulate time
@@ -162,13 +163,19 @@ func NewReflectorWithReset(lw ListerWatcher, expectedType interface{}, store Sto
 
 // NewNamedReflector same as NewReflector, but with a specified name for logging
 func NewNamedReflector(name string, lw ListerWatcher, expectedType interface{}, store Store, resyncPeriod time.Duration, allowPartialWatch bool) *Reflector {
+	realClock := &clock.RealClock{}
 	r := &Reflector{
 		name:                name,
 		listerWatcher:       lw,
 		store:               store,
-		period:              time.Second,
-		resyncPeriod:        resyncPeriod,
-		clock:               &clock.RealClock{},
+
+		// We used to make the call every 1sec (1 QPS), the goal here is to achieve ~98% traffic reduction when
+		// API server is not healthy. With these parameters, backoff will stop at [30,60) sec interval which is
+		// 0.22 QPS. If we don't backoff for 2min, assume API server is healthy and we reset the backoff.
+		backoffManager: wait.NewExponentialBackoffManager(800*time.Millisecond, 30*time.Second, 2*time.Minute, 2.0, 1.0, realClock),
+		resyncPeriod:   resyncPeriod,
+		clock:          realClock,
+
 		filterBounds:        make([]filterBound, 0),
 		aggChan:             make(chan interface{}),
 		clientSetUpdateChan: apiserverupdate.WatchClientSetUpdate(),
@@ -208,8 +215,8 @@ var internalPackages = []string{"client-go/tools/cache/"}
 // Run starts a watch and handles watch events. Will restart the watch if it is closed.
 // Run will exit when stopCh is closed.
 func (r *Reflector) Run(stopCh <-chan struct{}) {
-	klog.V(3).Infof("Starting reflector %v (%s) from %s", r.expectedTypeName, r.resyncPeriod, r.name)
-	wait.Until(func() {
+	klog.V(2).Infof("Starting reflector %s (%s) from %s", r.expectedTypeName, r.resyncPeriod, r.name)
+	wait.BackoffUntil(func() {
 		if len(r.filterBounds) == 0 {
 			if err := r.ListAndWatch(stopCh); err != nil {
 				utilruntime.HandleError(err)
@@ -246,7 +253,8 @@ func (r *Reflector) Run(stopCh <-chan struct{}) {
 				}
 			}
 		}
-	}, r.period, stopCh)
+	}, r.backoffManager, true, stopCh)
+	klog.V(2).Infof("Stopping reflector %s (%s) from %s", r.expectedTypeName, r.resyncPeriod, r.name)
 }
 
 var (
