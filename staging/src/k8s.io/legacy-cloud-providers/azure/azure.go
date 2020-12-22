7bb7290cc0ee07e478871d0abd6c7004153d05e4
sonyafenge <sonyafenge@gmail.com>
2020-12-22 02:59:34 +0000

    fix build error

--- staging/src/k8s.io/legacy-cloud-providers/azure/azure.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/azure.go
@@ -185,7 +185,7 @@ type Cloud struct {
 	DisksClient             DisksClient
 	SnapshotsClient         *compute.SnapshotsClient
 	FileClient              FileClient
-	resourceRequestBackoff  wait.Backoff
+	ResourceRequestBackoff  wait.Backoff
 	metadata                *InstanceMetadataService
 	vmSet                   VMSet
 
@@ -406,7 +406,7 @@ func (az *Cloud) InitializeCloudFromConfig(config *Config, fromSecret bool) erro
 
 	az.Config = *config
 	az.Environment = *env
-	az.resourceRequestBackoff = resourceRequestBackoff
+	az.ResourceRequestBackoff = resourceRequestBackoff
 	az.metadata, err = NewInstanceMetadataService(metadataURL)
 	if err != nil {
 		return err
