ff6bb9e700d3ae226cf2a9f9053a6050d3b5ca46
Ying Huang <sindica2000@yahoo.com>
2021-04-08 11:29:43 -0700

    Add log for percentageOfNodesToScore vs. evaluated node (#1070)

--- pkg/scheduler/core/generic_scheduler.go
+++ pkg/scheduler/core/generic_scheduler.go
@@ -459,6 +459,8 @@ func (g *genericScheduler) numFeasibleNodesToFind(numAllNodes int32) (numNodes i
 		return minFeasibleNodesToFind
 	}
 
+	klog.V(2).Infof("Get # of evaluated node. Total nodes %v, percentageOfNodesToScore %v, adaptivePercentage %v, node to evaluate %v",
+		numAllNodes, g.percentageOfNodesToScore, adaptivePercentage, numNodes)
 	return numNodes
 }
 
