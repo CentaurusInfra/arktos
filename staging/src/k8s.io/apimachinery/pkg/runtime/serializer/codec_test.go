cf3d8cda8f4e279fb9133c376e53fd3427ac4858
Ying Huang <sindica2000@yahoo.com>
2021-05-05 18:48:42 +0000

    update-bazel & copyright header

--- staging/src/k8s.io/apimachinery/pkg/runtime/serializer/codec_test.go
+++ staging/src/k8s.io/apimachinery/pkg/runtime/serializer/codec_test.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2014 The Kubernetes Authors.
+Copyright 2020 Authors of Arktos - file modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
