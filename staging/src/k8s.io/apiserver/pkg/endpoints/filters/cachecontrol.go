801ceac5af14f7e9c76b36ec6eee6e0a5700f2b9
Ying Huang <sindica2000@yahoo.com>
2021-05-06 23:06:07 +0000

    update-bazel&copyright

--- staging/src/k8s.io/apiserver/pkg/endpoints/filters/cachecontrol.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/filters/cachecontrol.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// File modified by cherrypick from kubernetes on 05/06/2021
 package filters
 
 import (
