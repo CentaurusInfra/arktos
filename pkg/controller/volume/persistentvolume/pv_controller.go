b042066cb80a15f4d75052fb8787b671dbe3021f
Ying Huang <sindica2000@yahoo.com>
2021-05-07 01:51:35 +0000

    K8s 1.18 PR 79971 - add check to reduce orphaned volume . Cleaner history of pv_controller merging . Performance improvement

--- pkg/controller/volume/persistentvolume/pv_controller.go
+++ pkg/controller/volume/persistentvolume/pv_controller.go
@@ -1416,6 +1416,10 @@ func (ctrl *PersistentVolumeController) provisionClaimOperation(
 
 	pvName := ctrl.getProvisionedVolumeNameForClaim(claim)
 	volume, err := ctrl.kubeClient.CoreV1().PersistentVolumesWithMultiTenancy(claim.Tenant).Get(pvName, metav1.GetOptions{})
+	if err != nil && !apierrs.IsNotFound(err) {
+		klog.V(3).Infof("error reading persistent volume %q: %v", pvName, err)
+		return pluginName, err
+	}
 	if err == nil && volume != nil {
 		// Volume has been already provisioned, nothing to do.
 		klog.V(4).Infof("provisionClaimOperation [%s]: volume already exists, skipping", claimToClaimKey(claim))
