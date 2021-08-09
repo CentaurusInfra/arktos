669e5c10df7a30a275432e9887aec825309ebef6
Ying Huang <sindica2000@yahoo.com>
2021-08-09 22:11:39 +0000

    Set watch as never expire

--- staging/src/k8s.io/client-go/tools/cache/shared_informer.go
+++ staging/src/k8s.io/client-go/tools/cache/shared_informer.go
@@ -140,7 +140,7 @@ func NewSharedIndexInformer(lw ListerWatcher, objType runtime.Object, defaultEve
 		indexer:                         NewIndexer(DeletionHandlingMetaNamespaceKeyFunc, indexers),
 		listerWatcher:                   lw,
 		objectType:                      objType,
-		resyncCheckPeriod:               defaultEventHandlerResyncPeriod,
+		resyncCheckPeriod:               0,
 		defaultEventHandlerResyncPeriod: defaultEventHandlerResyncPeriod,
 		cacheMutationDetector:           NewCacheMutationDetector(fmt.Sprintf("%T", objType)),
 		clock:                           realClock,
