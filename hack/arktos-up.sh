d2fbe9f467825beea2cdc10a584354c5b080fe53
click2cloud-alpha1 <click2cloud-alpha1@click2cloud.net>
2021-08-24 11:48:54 +0530

    added support for flannel cni in arktos-up

--- hack/arktos-up.sh
+++ hack/arktos-up.sh
@@ -64,8 +64,8 @@ if [[ ! -e "${CONTAINERD_SOCK_PATH}" ]]; then
   exit 1
 fi
 
-# Install simple cni plugin based on env var CNIPLUGIN (bridge, alktron) before cluster is up.
-# If more advanced cni like Flannel is desired, it should be installed AFTER the clsuter is up;
+# Install simple cni plugin based on env var CNIPLUGIN (bridge, alktron, flannel) before cluster is up.
+# If more advanced cni like Flannel is desired, it should be installed AFTER the cluster is up;
 # in that case, please set ARKTOS-NO-CNI_PREINSTALLED to any no-empty value
 source ${KUBE_ROOT}/hack/arktos-cni.rc
 
@@ -198,6 +198,11 @@ cleanup()
   if [[ -e "${VIRTLET_LOG_DIR}" ]]; then
        echo "Cleanup runtime log folder"
        rm -f -r "${VIRTLET_LOG_DIR}"
+  fi
+
+  # Kill arktos-network-controller process
+  if [[ "${CNIPLUGIN}" == "flannel" ]] && [[ ! "$(ps -ax|grep arktos-network-controller | grep -v grep | wc -l)" -eq 0 ]]; then
+    sudo killall arktos-network-controller 2>/dev/null
   fi
 
   exit 0
@@ -514,6 +519,12 @@ if [[ "${START_MODE}" != "nokubelet" ]]; then
         print_color "Unsupported host OS.  Must be Linux or Mac OS X, kubelet aborted."
         ;;
     esac
+fi
+
+# Applying flannel cni
+if [[ "${CNIPLUGIN}" = "flannel" ]]; then
+  ${KUBECTL} --kubeconfig="${CERT_DIR}/admin.kubeconfig" apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+  ${KUBE_ROOT}/_output/local/bin/linux/amd64/arktos-network-controller --kubeconfig=/var/run/kubernetes/admin.kubeconfig --kube-apiserver-ip="$(hostname -I | awk '{print $1}')" > /tmp/arktos-network-controller.log 2>&1 &
 fi
 
 if [[ -n "${PSP_ADMISSION}" && "${AUTHORIZATION_MODE}" = *RBAC* ]]; then
