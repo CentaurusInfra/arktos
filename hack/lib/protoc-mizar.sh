6d66a5be8fa9b7a66687e3dd17f7c11a775e5c0b
q131172019 <q131172019@gmail.com>
2022-01-18 04:26:40 +0000

    Code changes based on HongWei's feedback and YingHuang's feedback

--- hack/lib/protoc-mizar.sh
+++ hack/lib/protoc-mizar.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# This script is created based on hack/lib/protoc.sh and is made with minor changes
+#
+
 set -o errexit
 set -o nounset
 set -o pipefail
