a65f5927547c559e9f91395ca4a4ae4d5b7a7a43
Yunwen Bai <yunwen.bai@futurewei.com>
2022-01-23 21:17:58 -0800

    fix type mismatch

--- staging/src/k8s.io/apiserver/pkg/util/openstack/types.go
+++ staging/src/k8s.io/apiserver/pkg/util/openstack/types.go
@@ -17,55 +17,26 @@ limitations under the License.
 package openstack
 
 import (
-	"k8s.io/kubernetes/pkg/apis/apps"
 	"strconv"
 
-	api "k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/klog"
+	v12 "k8s.io/api/apps/v1"
 )
-
-/*
-{
-  "spec":{
-    "template":{
-
-      "spec":{
-        "virtualMachine":{
-          "image":"%s",
-          "imagePullPolicy":"IfNotPresent",
-          "keyPairName":"foobar",
-          "name":"%s",
-          "resources":{
-            "limits":{
-              "cpu":"%d",
-              "memory":"%dMi"
-            },
-            "requests":{
-              "cpu":"%d",
-              "memory":"%dMi"
-            }
-          }
-        }
-      }
-    }
-  }
-}
- */
 
 type batchRequestBody struct {
 	ApiVersion string            `json:"apiVersion"`
 	Kind       string            `json:"kind"`
 	MetaData   metav1.ObjectMeta `json:"metadata"`
-	Spec       apps.ReplicaSetSpec        `json:"spec"`
+	Spec       v12.ReplicaSetSpec       `json:"spec"`
 }
 
 func getReplicasetRequestBody(replicas int, serverName, imageRef string, vcpu, memInMi int) (string, error) {
 	t := batchRequestBody{}
-	t.ApiVersion = "v1"
+	t.ApiVersion = "apps/v1"
 	t.Kind = "ReplicaSet"
 	t.MetaData = metav1.ObjectMeta{
 		Name:        serverName,
@@ -73,28 +44,31 @@ func getReplicasetRequestBody(replicas int, serverName, imageRef string, vcpu, m
 		Tenant:      "system",
 	}
 
-	t.Spec = apps.ReplicaSetSpec{
-		Replicas: int32(replicas),
-		Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"ln": serverName}},
-		Template: api.PodTemplateSpec{
+	i := int32(replicas)
+	t.Spec = v12.ReplicaSetSpec{
+		Replicas: &i,
+		Selector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{"ln": serverName},
+	},
+		Template: v1.PodTemplateSpec{
 			ObjectMeta: metav1.ObjectMeta{
 				Annotations: map[string]string{"VirtletCPUModel": "host-model"},
 				Labels: map[string]string{"ln": serverName},
 			},
-			Spec: api.PodSpec{
-				VirtualMachine: &api.VirtualMachine{
+			Spec: v1.PodSpec{
+				VirtualMachine: &v1.VirtualMachine{
 					Image:       imageRef,
 					KeyPairName: "foobar",
 					Name:        serverName,
 					PublicKey:   "ssh-rsa AAA",
-					Resources: api.ResourceRequirements{
-						Limits: api.ResourceList{
-							api.ResourceCPU:    resource.MustParse(strconv.Itoa(vcpu)),
-							api.ResourceMemory: resource.MustParse(strconv.Itoa(memInMi) + "Mi"),
+					Resources: v1.ResourceRequirements{
+						Limits: v1.ResourceList{
+							v1.ResourceCPU:    resource.MustParse(strconv.Itoa(vcpu)),
+							v1.ResourceMemory: resource.MustParse(strconv.Itoa(memInMi) + "Mi"),
 						},
-						Requests: api.ResourceList{
-							api.ResourceCPU:    resource.MustParse(strconv.Itoa(vcpu)),
-							api.ResourceMemory: resource.MustParse(strconv.Itoa(memInMi) + "Mi"),
+						Requests: v1.ResourceList{
+							v1.ResourceCPU:    resource.MustParse(strconv.Itoa(vcpu)),
+							v1.ResourceMemory: resource.MustParse(strconv.Itoa(memInMi) + "Mi"),
 						},
 					},
 				},
