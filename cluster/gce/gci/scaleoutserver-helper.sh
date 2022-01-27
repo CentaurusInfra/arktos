2fd96b72fd567054b55fc38331bc929dbc66415e
q131172019 <q131172019@gmail.com>
2022-01-27 21:37:27 +0000

    Code changes for ARKTOS_NETWORK_TEMPLATE

--- cluster/gce/gci/scaleoutserver-helper.sh
+++ cluster/gce/gci/scaleoutserver-helper.sh
@@ -160,7 +160,9 @@ function create-scaleoutserver-instance-internal() {
   metadata="${metadata},kube-master-certs=${KUBE_TEMP}/kube-master-certs.yaml"
   metadata="${metadata},cluster-location=${KUBE_TEMP}/cluster-location.txt"
   metadata="${metadata},controllerconfig=${KUBE_TEMP}/controllerconfig.json"
-  metadata="${metadata},networktemplate=${KUBE_TEMP}/network.tmpl"
+  if [[ -s ${KUBE_TEMP}/network.tmpl ]]; then
+    metadata="${metadata},networktemplate=${KUBE_TEMP}/network.tmpl"
+  fi
   metadata="${metadata},${MASTER_EXTRA_METADATA}"
 
   local disk="name=${server_name}-pd"
