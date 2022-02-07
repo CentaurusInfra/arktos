abe8ee2882276fed228756ac1a88be7a68c8b021
Ying Huang <sindica2000@yahoo.com>
2022-02-07 13:40:01 -0800

    Use builtin json struct instead of template files for Mizar VPC (#1342)
    * Use builtin json struct instead of template files for Mizar VPC
    . Randomize VPC/Subnet ip range
    . Add UT for VPC/Subnet spec
    . Simplify network creation for mizar-arktos-network-controller
    . VPC/subnet can be created before network is ready.
    . Remove redundant log
    . Add retry when vpc/subnet creation has error.
    
    * Remove mizar template file. Needs to go together with kube-up related changes.
    This is a demo only used to show the code works w/o template files.
    
    * Use consist message
    
    * Revert "[kube-up][scale-out]Automatically create default mizar vpc and subnet for tenants  (#1327)"
    
    This reverts commit 8a2c692c35aae1b658404df5feb9bcc5349c748b.
    
    * Increase VPC/subnet IP range for CR.

--- pkg/controller/mizar/util.go
+++ pkg/controller/mizar/util.go
@@ -1,16 +1,3 @@
-/*
-Copyright 2020 Authors of Arktos.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package mizar
 
 import (
@@ -40,47 +27,6 @@ type KeyWithEventType struct {
 	EventType       EventType
 	Key             string
 	ResourceVersion string
-}
-
-type MizarNetworkPolicyPortSelector struct {
-	Protocol string `json:"protocol"`
-	Port     string `json:"port"`
-}
-
-type MizarNetworkPolicyPodSelector struct {
-	MatchLabels map[string]string `json:"matchLabels,omitempty"`
-}
-
-type MizarNetworkPolicyNamespaceSelector struct {
-	MatchLabels map[string]string `json:"matchLabels,omitempty"`
-}
-
-type MizarNetworkPolicyIPBlock struct {
-	Cidr   string   `json:"cidr,omitempty"`
-	Except []string `json:"except,omitempty"`
-}
-
-type MizarNetworkPolicyRule struct {
-	P MizarNetworkPolicyPodSelector       `json:"podSelector,omitempty"`
-	N MizarNetworkPolicyNamespaceSelector `json:"namespaceSelector,omitempty"`
-	I MizarNetworkPolicyIPBlock           `json:"ipBlock,omitempty"`
-}
-
-type MizarNetworkPolicyIngressMsg struct {
-	Ports []MizarNetworkPolicyPortSelector `json:"ports"`
-	From  []MizarNetworkPolicyRule         `json:"from"`
-}
-
-type MizarNetworkPolicyEgressMsg struct {
-	Ports []MizarNetworkPolicyPortSelector `json:"ports"`
-	To    []MizarNetworkPolicyRule         `json:"to"`
-}
-
-type MizarNetworkPolicyPolicySpecMsg struct {
-	PodSel MizarNetworkPolicyPodSelector  `json:"podSelector,omitempty"`
-	In     []MizarNetworkPolicyIngressMsg `json:"ingress,omitempty"`
-	Out    []MizarNetworkPolicyEgressMsg  `json:"egress,omitempty"`
-	Type   []string                       `json:"policyTypes,omitempty"`
 }
 
 type StartHandler func(interface{}, string)
