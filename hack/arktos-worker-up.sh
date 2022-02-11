56122e151875246dc64a822bebd0dc4a4635e0d5
q131172019 <q131172019@gmail.com>
2022-02-11 19:30:20 +0000

    Add workers in scale-up U1804&U2004 and scale-out U2004

--- hack/arktos-worker-up.sh
+++ hack/arktos-worker-up.sh
@@ -17,27 +17,90 @@
 set -e
 die() { echo "$*" 1>&2 ; exit 1; }
 
-if [[ ! -n "${KUBELET_IP}" ]]; then
-    die "KUBELET_IP env var not set"
+echo "DBG: Flannel CNI plugin will be installed AFTER cluster is up"
+[ "${CNIPLUGIN}" == "flannel" ] && ARKTOS_NO_CNI_PREINSTALLED="y"
+
+OPT_CNI_BIN=/opt/cni/bin
+ETC_CNI_NET_DIR=/etc/cni/net.d
+echo "Clean up two directories $OPT_CNI_BIN and $ETC_CNI_NET_DIR ......"
+
+if [[ ! -d "${ETC_CNI_NET_DIR}" ]]; then
+   mkdir -p "${ETC_CNI_NET_DIR}" &>/dev/null || sudo mkdir -p "${ETC_CNI_NET_DIR}"
 fi
+CNI_SUDO=$(test -w "${ETC_CNI_NET_DIR}" || echo "sudo")
+
+if ${CNI_SUDO} test -f $ETC_CNI_NET_DIR/bridge.conf; then
+  ${CNI_SUDO} rm -f $ETC_CNI_NET_DIR/bridge.conf
+fi
+if ${CNI_SUDO} test -f $ETC_CNI_NET_DIR/10-flannel.conflist; then
+  ${CNI_SUDO} rm -f $ETC_CNI_NET_DIR/10-flannel.conflist
+fi
+if ${CNI_SUDO} test -f $ETC_CNI_NET_DIR/10-mizarcni.conf; then
+  ${CNI_SUDO} rm -f $ETC_CNI_NET_DIR/10-mizarcni.conf
+fi
+${CNI_SUDO} ls -alg $ETC_CNI_NET_DIR
+
+if [[ ! -d "${OPT_CNI_BIN}" ]]; then
+   mkdir -p "${OPT_CNI_BIN}" &>/dev/null || sudo mkdir -p "${OPT_CNI_BIN}"
+fi
+${CNI_SUDO} rm -f $OPT_CNI_BIN/*
+${CNI_SUDO} ls -alg $OPT_CNI_BIN
 
 # install cni plugin related packages
 KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+source ${KUBE_ROOT}/hack/lib/common-var-init.sh
 source ${KUBE_ROOT}/hack/arktos-cni.rc
 
-mkdir -p /tmp/arktos
+# creates a self-contained kubeconfig: args are sudo, dest-dir, ca file, host, port, client id, token(optional)
+function write_client_kubeconfig {
+    local sudo=$1
+    local dest_dir=$2
+    local ca_file=$3
+    local api_host=$4
+    local api_port=$5
+    local client_id=$6
+    local token=${7:-}
+    local protocol=${8:-"http"}
 
-SECRET_FOLDER=${SECRET_FOLDER:-"/tmp/arktos"}
-KUBELET_KUBECONFIG=${KUBELET_KUBECONFIG:-"${SECRET_FOLDER}/kubelet.kubeconfig"}
-KUBELET_CLIENTCA=${KUBELET_CLIENTCA:-"${SECRET_FOLDER}/client-ca.crt"}
+    cat <<EOF | ${sudo} tee "${dest_dir}"/"${client_id}".kubeconfig > /dev/null
+apiVersion: v1
+kind: Config
+clusters:
+  - cluster:
+      server: ${protocol}://${api_host}:${api_port}/
+    name: local-up-cluster
+users:
+  - user:
+    name: local-up-cluster
+contexts:
+  - context:
+      cluster: local-up-cluster
+      user: local-up-cluster
+    name: local-up-cluster
+current-context: local-up-cluster
+EOF
+}
+
+
+# Ensure CERT_DIR is created for auto-generated kubeconfig
+mkdir -p "${CERT_DIR}" &>/dev/null || sudo mkdir -p "${CERT_DIR}"
+CONTROLPLANE_SUDO=$(test -w "${CERT_DIR}" || echo "sudo -E")
+
+# Generate kubeconfig for kubelet
+write_client_kubeconfig "${CONTROLPLANE_SUDO}" "${CERT_DIR}" "" "${API_HOST}" "${API_PORT}" kubelet "" "http"
+${CONTROLPLANE_SUDO} chown "$(whoami)" "${CERT_DIR}/kubelet.kubeconfig"
+# Generate kubeconfig for kube-proxy
+write_client_kubeconfig "${CONTROLPLANE_SUDO}" "${CERT_DIR}" "" "${API_HOST}" "${API_PORT}" kube-proxy "" "http"
+${CONTROLPLANE_SUDO} chown "$(whoami)" "${CERT_DIR}/kube-proxy.kubeconfig"
+
+KUBELET_CLIENTCA=${KUBELET_CLIENTCA:-"${CERT_DIR}/client-ca.crt"}
+${CONTROLPLANE_SUDO} chown "$(whoami)" "${KUBELET_CLIENTCA}"
+
 HOSTNAME_OVERRIDE=${HOSTNAME_OVERRIDE:-"$(hostname)"}
 CLUSTER_DNS=${CLUSTER_DNS:-"10.0.0.10"}
-
-if [[ ! -s "${KUBELET_KUBECONFIG}" ]]; then
-    echo "kubelet kubeconfig file not found at ${KUBELET_KUBECONFIG}."
-    echo "Please copy this file from Arktos master, e.g. in GCP run:"
-    echo "gcloud compute scp <master-vm-name>:/var/run/kubernetes/kubelet.kubeconfig /tmp/arktos/"
-    die "arktos worker node failed to start."
+FEATURE_GATES="${FEATURE_GATES_COMMON_BASE}"
+if [ -z ${DISABLE_NETWORK_SERVICE_SUPPORT} ]; then
+  FEATURE_GATES="${FEATURE_GATES_COMMON_BASE},MandatoryArktosNetwork=true"
 fi
 
 if [[ ! -s "${KUBELET_CLIENTCA}" ]]; then
@@ -47,29 +110,140 @@ if [[ ! -s "${KUBELET_CLIENTCA}" ]]; then
     die "arktos worker node failed to start."
 fi
 
-make all WHAT=cmd/hyperkube
+make all WHAT="cmd/hyperkube cmd/kubelet cmd/kube-proxy"
 
-sudo ./_output/local/bin/linux/amd64/hyperkube kubelet \
---v=3 \
---container-runtime=remote \
---hostname-override=${HOSTNAME_OVERRIDE} \
---address=${KUBELET_IP} \
---kubeconfig=${KUBELET_KUBECONFIG} \
---authorization-mode=Webhook \
---authentication-token-webhook \
---client-ca-file=${KUBELET_CLIENTCA} \
---feature-gates=AllAlpha=false \
---cpu-cfs-quota=true \
---enable-controller-attach-detach=true \
---cgroups-per-qos=true \
---cgroup-driver= --cgroup-root= \
---cluster-dns=${CLUSTER_DNS} \
---cluster-domain=cluster.local \
---container-runtime-endpoint="containerRuntime,container,/run/containerd/containerd.sock;vmRuntime,vm,/run/virtlet.sock" \
---runtime-request-timeout=2m \
---port=10250 \
---resolv-conf=/run/systemd/resolve/resolv.conf \
-> /tmp/kubelet.worker.log 2>&1 &
+# Check if the kubelet is still running
+echo "DBG: Cleaning old processes for kubelet"
+KUBELET_PID=`ps -ef |grep kubelet |grep -v grep |head -1 |awk '{print $2}'`
+[[ -n "${KUBELET_PID-}" ]] && mapfile -t KUBELET_PIDS < <(pgrep -P "${KUBELET_PID}"; ps -o pid= -p "${KUBELET_PID}")
+[[ -n "${KUBELET_PIDS-}" ]] && sudo kill "${KUBELET_PIDS[@]}" 2>/dev/null
+
+# Check if the proxy is still running
+echo "DBG: Cleaning old processes for kube-proxy"
+PROXY_PID=`ps -ef |grep kube-proxy |grep -v grep |head -1 |awk '{print $2}'`
+[[ -n "${PROXY_PID-}" ]] && mapfile -t PROXY_PIDS < <(pgrep -P "${PROXY_PID}"; ps -o pid= -p "${PROXY_PID}")
+[[ -n "${PROXY_PIDS-}" ]] && sudo kill "${PROXY_PIDS[@]}" 2>/dev/null
+
+# Check if the flannel is still running
+echo "DBG: Cleaning old processes for flannel"
+FLANNELD_PID=`ps -ef |grep flannel |grep -v grep |head -1 |awk '{print $2}'`
+[[ -n "${FLANNELD_PID-}" ]] && mapfile -t FLANNELD_PIDS < <(pgrep -P "${FLANNELD_PID}"; ps -o pid= -p "${FLANNELD_PID}") 
+[[ -n "${FLANNELD_PIDS-}" ]] && sudo kill "${FLANNELD_PIDS[@]}" 2>/dev/null
+
+PROXY_TP_KUBECONFIGS=""
+
+if [[ "${IS_SCALE_OUT}" != "true" ]]; then
+  echo "IS_SCALE_OUT false"
+
+  sudo ./_output/local/bin/linux/amd64/hyperkube kubelet \
+  --v="${LOG_LEVEL}" \
+  --container-runtime=remote \
+  --hostname-override=${HOSTNAME_OVERRIDE} \
+  --address='0.0.0.0' \
+  --kubeconfig="${CERT_DIR}/kubelet.kubeconfig" \
+  --authorization-mode=Webhook \
+  --authentication-token-webhook \
+  --client-ca-file=${KUBELET_CLIENTCA} \
+  --feature-gates=${FEATURE_GATES} \
+  --cpu-cfs-quota=true \
+  --enable-controller-attach-detach=true \
+  --cgroups-per-qos=true \
+  --cgroup-driver= --cgroup-root= \
+  --cluster-dns=${CLUSTER_DNS} \
+  --cluster-domain=cluster.local \
+  --container-runtime-endpoint="containerRuntime,container,/run/containerd/containerd.sock;vmRuntime,vm,/run/virtlet.sock" \
+  --runtime-request-timeout=2m \
+  --port=10250 \
+  --resolv-conf=/run/systemd/resolve/resolv.conf \
+  > /tmp/kubelet.worker.log 2>&1 &
+
+else
+
+  echo "IS_SCALE_OUT true"
+  if  [[ -z "${API_TENANT_SERVER}" ]]; then
+    echo ERROR: Please set API_TENANT_SERVER. For example: API_TENANT_SERVER=192.168.0.2 or API_TENANT_SERVER=192.168.0.2,192.168.0.5
+    exit 1
+  else
+    KUBELET_TENANT_SERVER_KUBECONFIG_FLAG="--tenant-server-kubeconfig="
+    kubeconfig_filename="tenant-server-kubelet"
+
+    API_TENANT_SERVERS=(${API_TENANT_SERVER//,/ })
+    serverCount=${#API_TENANT_SERVERS[@]}
+    for (( pos=0; pos<${serverCount}; pos++ ));
+    do
+      # generate kubeconfig fpr tenant partitions
+      write_client_kubeconfig "${CONTROLPLANE_SUDO}" "${CERT_DIR}" "" "${API_TENANT_SERVERS[${pos}]}" "${API_PORT}" ${kubeconfig_filename} "" "http"
+      ${CONTROLPLANE_SUDO} mv "${CERT_DIR}/${kubeconfig_filename}.kubeconfig" "${CERT_DIR}/${kubeconfig_filename}${pos}.kubeconfig"
+      ${CONTROLPLANE_SUDO} chown "$(whoami)" "${CERT_DIR}/${kubeconfig_filename}${pos}.kubeconfig"
+
+      KUBELET_TENANT_SERVER_KUBECONFIG_FLAG="${KUBELET_TENANT_SERVER_KUBECONFIG_FLAG}${CERT_DIR}/${kubeconfig_filename}${pos}.kubeconfig,"
+      PROXY_TP_KUBECONFIGS="${PROXY_TP_KUBECONFIGS}${CERT_DIR}/${kubeconfig_filename}${pos}.kubeconfig,"
+    done
+    KUBELET_TENANT_SERVER_KUBECONFIG_FLAG=${KUBELET_TENANT_SERVER_KUBECONFIG_FLAG::-1}
+    PROXY_TP_KUBECONFIGS=${PROXY_TP_KUBECONFIGS::-1}
+  fi
+
+  sudo ./_output/local/bin/linux/amd64/hyperkube kubelet \
+  --v="${LOG_LEVEL}" \
+  --container-runtime=remote \
+  --hostname-override=${HOSTNAME_OVERRIDE} \
+  --address='0.0.0.0' \
+  --kubeconfig="${CERT_DIR}/kubelet.kubeconfig" \
+  ${KUBELET_TENANT_SERVER_KUBECONFIG_FLAG} \
+  --authorization-mode=Webhook \
+  --authentication-token-webhook \
+  --client-ca-file=${KUBELET_CLIENTCA} \
+  --feature-gates=${FEATURE_GATES} \
+  --cpu-cfs-quota=true \
+  --enable-controller-attach-detach=true \
+  --cgroups-per-qos=true \
+  --cgroup-driver= --cgroup-root= \
+  --cluster-dns=${CLUSTER_DNS} \
+  --cluster-domain=cluster.local \
+  --container-runtime-endpoint="containerRuntime,container,/run/containerd/containerd.sock;vmRuntime,vm,/run/virtlet.sock" \
+  --runtime-request-timeout=2m \
+  --port=10250 \
+  --resolv-conf=/run/systemd/resolve/resolv.conf \
+  > /tmp/kubelet.worker.log 2>&1 &
+
+fi
 
 echo "kubelet has been started. please check /tmp/kubelet.worker.log for its running log."
 
+if [ "${CNIPLUGIN}" == "flannel" ]; then
+    echo "DBG: Installing Flannel cni plugin..."
+    sleep 30  #need sometime for KCM to be fully functioning
+    if [ "${IS_SCALE_OUT}" == "true" ]; then
+       install_flannel "${RESOURCE_PARTITION_POD_CIDR}" "${API_HOST}"
+    else
+       install_flannel "${KUBE_CONTROLLER_MANAGER_CLUSTER_CIDR}" "${API_HOST}"
+    fi
+fi
+
+# Start proxy
+cat <<EOF > /tmp/kube-proxy.yaml
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+clientConnection:
+  kubeconfig: ${CERT_DIR}/kube-proxy.kubeconfig
+tenantPartitionKubeConfig: ${PROXY_TP_KUBECONFIGS}
+hostnameOverride: ${HOSTNAME_OVERRIDE}
+mode: ${KUBE_PROXY_MODE}
+EOF
+
+if [[ -n ${FEATURE_GATES} ]]; then
+  echo "featureGates:"
+  # Convert from foo=true,bar=false to
+  #   foo: true
+  #   bar: false
+  for gate in $(echo "${FEATURE_GATES}" | tr ',' ' '); do
+    echo "${gate}" | sed -e 's/\(.*\)=\(.*\)/  \1: \2/'
+  done
+fi >>/tmp/kube-proxy.yaml
+
+sudo ./_output/local/bin/linux/amd64/hyperkube kube-proxy \
+  --v="${LOG_LEVEL}" \
+  --config=/tmp/kube-proxy.yaml \
+  --master="http://${API_HOST}:${API_PORT}" > "${LOG_DIR}/kube-proxy.log" 2>&1 &
+
+echo "kube proxy has been started. please check /tmp/kube-proxy.log for its running log."
