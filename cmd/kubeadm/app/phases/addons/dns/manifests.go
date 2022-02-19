30f1d70ed984c30a86c6170946f97897a9722d9c
sonyafenge <45471193+sonyafenge@users.noreply.github.com>
2022-02-18 18:56:00 -0800

    kube-up.sh containerd and Daemonset support (#1366)
    * add node tags for rp minion
    
    * add workaround for klog issue causing CoreDNS to crash
    
    * kube-up.sh support Daemonset for scale-out (#1271)
    
    * scaleout tp server enable  daemonset
    
    * rp kubelet add tenant-server-kubeconfig information
    
    * remove daemonset from RP server
    
    * Change kube-up.sh to use Containerd as default runtime (#1273)
    
    * add install-docker and install-containerd for ubuntu
    
    * change default runtime to contanerd
    
    * remove unused LOG_DUMP_SYSTEMD_SERVICES from config
    
    Co-authored-by: Sandeep Rajan <srajan@infoblox.com>

--- cmd/kubeadm/app/phases/addons/dns/manifests.go
+++ cmd/kubeadm/app/phases/addons/dns/manifests.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2017 The Kubernetes Authors.
+Copyright 2020 Authors of Arktos - file modified.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -260,6 +261,8 @@ spec:
         - name: config-volume
           mountPath: /etc/coredns
           readOnly: true
+        - name: tmp
+          mountPath: /tmp
         ports:
         - containerPort: 53
           name: dns
@@ -294,6 +297,8 @@ spec:
           readOnlyRootFilesystem: true
       dnsPolicy: Default
       volumes:
+        - name: tmp
+          emptyDir: {}
         - name: config-volume
           configMap:
             name: coredns
