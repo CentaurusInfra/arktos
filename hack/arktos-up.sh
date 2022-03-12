db28c734e60fd27521bd9cfa78de75cbe328310c
Ying Huang <sindica2000@yahoo.com>
2022-03-12 02:08:05 +0000

    update mizar version

--- hack/arktos-up.sh
+++ hack/arktos-up.sh
@@ -556,8 +556,7 @@ fi
 # Applying mizar cni
 if [[ "${CNIPLUGIN}" == "mizar" ]]; then
   ${KUBECTL} --kubeconfig="${CERT_DIR}/admin.kubeconfig" create configmap system-source --namespace=kube-system --from-literal=name=arktos --from-literal=company=futurewei
-  # ${KUBECTL} --kubeconfig="${CERT_DIR}/admin.kubeconfig" apply -f ${KUBE_ROOT}/hack/testdata/mizar/deploy.mizar.next.yaml 
-  ${KUBECTL} --kubeconfig="${CERT_DIR}/admin.kubeconfig" apply -f https://raw.githubusercontent.com/CentaurusInfra/mizar/dev-next/etc/deploy/deploy.mizar.dev.yaml
+  ${KUBECTL} --kubeconfig="${CERT_DIR}/admin.kubeconfig" apply -f https://raw.githubusercontent.com/CentaurusInfra/mizar/v0.9/etc/deploy/deploy.mizar.yaml
 fi
 
 if [[ -n "${PSP_ADMISSION}" && "${AUTHORIZATION_MODE}" = *RBAC* ]]; then
